3f1942fca996fac56805aea8886fe97043e09615
sathyra <105861021+sathyra@users.noreply.github.com>
2022-06-14 14:09:00 +0530

    Update L1.py

--- L1.py
+++ L1.py
@@ -1,75 +1,37 @@
-
 import numpy as np
 from tkinter import *
-
 root=Tk()
-root.geometry("600x400")   
-def sortarr():
-    
-    arr1 = np.array([3, 2, 0, 1])
-    arr2 = np.array(['banana', 'cherry', 'apple'])
+root.geometry("600x400")
+arr1 = np.array([3, 2, 0, 1])
+arr2 = np.array(['banana', 'cherry', 'apple'])
 
-    def srt_ar_asc():
-        #i = int(input1.get())
-        newarr3 = np.sort(arr1)
-        #print(INPUT1)
-        label.config(text=newarr3)
+#Defining the sorting function
+def srt_ar_asc():
+    newarr3 = np.sort(arr1)
+    label.config(text=newarr3)
 
-    def srt_ar_dsc():
-        #i = int(input1.get())
-        newarr3 = -np.sort(-arr1)
-        #print(INPUT1)
-        label.config(text=newarr3)
+def srt_ar_dsc():
+    newarr3 = -np.sort(-arr1)
+    label.config(text=newarr3)
 
-    def srt_txt_asc():
-        #i = int(input1.get())
-        newarr4 = np.sort(arr2)
-        #print(INPUT1)
-        label.config(text=newarr4)
+def srt_txt_asc():
+    newarr4 = np.sort(arr2)
+    label.config(text=newarr4)
 
-    Message(root, text= arr1,bg='light blue', font = 50).pack()
-    Button(root, text="Sort Array Ascending", command=srt_ar_asc).pack(pady=20)
+#display the array elements
+Message(root, text= arr1,bg='light blue', font = 50).pack()
 
-    Button(root, text="Sort Array Descending", command=srt_ar_dsc).pack(pady=20)
+#ascending button
+Button(root, text="Sort Array Ascending", command=srt_ar_asc).pack(pady=20)
 
-    Message(root, text= arr2,bg='light blue', font = 50).pack()
-    Button(root, text="Sort Text", command=srt_txt_asc).pack(pady=20)
-    label = Label(root,width = 35, bg='light blue', font = 50)
-    label.pack(pady=20)
-    # root.sortarr()
+#descending button
+Button(root, text="Sort Array Descending", command=srt_ar_dsc).pack(pady=20)
 
-    root.mainloop()
+#Button and text elements
+Message(root, text= arr2,bg='light blue', font = 50).pack()
+Button(root, text="Sort Text", command=srt_txt_asc).pack(pady=20)
 
-button = Button( text ='Sorting Array', bd = '5', bg = 'yellow',command = sortarr())
-
-### Main
-
-# def close():
-#     #top.destroy()
-#     root.destroy()
-
-# create a tkinter window
-#root = Tk()             
- 
-# Open window having dimension 100x100
-#root.geometry('500x200')
-#root.title('NUMPY')
-
-# button_dict = {}
-# options = [ 'Sorting Array']
-
-# for i in options:
-    # def func(i):
-        
-#         if i == 'Sorting Array':
-#             return sortarr
-        # elif i == 'Exit':
-        #     return close
-        
-# Create a Button
- 
-    # Set the position of button on the top of window.  
-#button.pack()   
- 
-#root.mainloop()
-
+#output is displayed using the label
+label = Label(root,width = 35, bg='light blue', font = 50)
+label.pack()
+root.mainloop()
